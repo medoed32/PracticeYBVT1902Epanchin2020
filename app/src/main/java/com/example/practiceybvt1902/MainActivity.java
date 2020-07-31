e8990a3d1fe8089fcd7e2d9efb4c257763aa0161
Nikita Epanchin <medoed32@gmail.com>
2020-07-31 18:51:05 +0300

    Add_Accel

--- app/src/main/java/com/example/practiceybvt1902/MainActivity.java
+++ app/src/main/java/com/example/practiceybvt1902/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.practiceybvt1902;
 
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -18,7 +24,6 @@ import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-
     TextView tvEnabledGPS;
     TextView tvStatusGPS;
     TextView tvLocationGPS;
@@ -26,28 +31,69 @@ public class MainActivity extends AppCompatActivity {
     TextView tvStatusNet;
     TextView tvLocationNet;
 
+    TextView tvLocationAccel;
+    SensorManager sensorManager;
+    Sensor sensorAccel;
+    Sensor sensorLinAccel;
+    Sensor sensorGravity;
+
     private LocationManager locationManager;
     StringBuilder sbGPS = new StringBuilder();
     StringBuilder sbNet = new StringBuilder();
+    StringBuilder sbAccel = new StringBuilder();
+
+    float[] valuesAccel = new float[3];
+    float[] valuesAccelMotion = new float[3];
+    float[] valuesAccelGravity = new float[3];
+    float[] valuesLinAccel = new float[3];
+    float[] valuesGravity = new float[3];
+
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Accel
+        tvLocationAccel = findViewById(R.id.tvLocationAccel);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorLinAccel = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        sensorGravity = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+
+        //GPS
         tvEnabledGPS = (TextView) findViewById(R.id.tvEnabledGPS);
         tvStatusGPS = (TextView) findViewById(R.id.tvStatusGPS);
         tvLocationGPS = (TextView) findViewById(R.id.tvLocationGPS);
         tvEnabledNet = (TextView) findViewById(R.id.tvEnabledNet);
         tvStatusNet = (TextView) findViewById(R.id.tvStatusNet);
         tvLocationNet = (TextView) findViewById(R.id.tvLocationNet);
-
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        //Accel
+        sensorManager.registerListener(listener, sensorAccel, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(listener, sensorLinAccel, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(listener, sensorGravity, SensorManager.SENSOR_DELAY_NORMAL);
+        timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        showInfo();
+                    }
+                });
+            }
+        };
+        timer.schedule(task, 0, 400);
+
+        //GPS
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -66,8 +112,53 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        sensorManager.unregisterListener(listener);
+        timer.cancel();
         locationManager.removeUpdates(locationListener);
     }
+
+    String format(float values[]) {
+        return String.format("%1$.1f\t\t%2$.1f\t\t%3$.1f", values[0], values[1], values[2]);
+    }
+
+    void showInfo() {
+        sbAccel.setLength(0);
+        sbAccel.append("Accelerometer: " + format(valuesAccel))
+                .append("\n\nAccel motion: " + format(valuesAccelMotion))
+                .append("\n\nAccel gravity: " + format(valuesAccelGravity))
+                .append("\n\nLin accel: " + format(valuesLinAccel))
+                .append("\n\nGravity: " + format(valuesGravity));
+        tvLocationAccel.setText(sbAccel);
+    }
+
+    SensorEventListener listener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            switch (event.sensor.getType()) {
+                case Sensor.TYPE_ACCELEROMETER:
+                    for (int i = 0; i < 3; i++) {
+                        valuesAccel[i] = event.values[i];
+                        valuesAccelGravity[i] = (float) (0.1 * event.values[i] + 0.9 * valuesAccelGravity[i]);
+                        valuesAccelMotion[i] = event.values[i] - valuesGravity[i];
+                    }
+                    break;
+                case Sensor.TYPE_LINEAR_ACCELERATION:
+                    for (int i = 0; i < 3; i++) {
+                        valuesLinAccel[i] = event.values[i];
+                    }
+                    break;
+                case Sensor.TYPE_GRAVITY:
+                    for (int i = 0; i < 3; i++) {
+                        valuesGravity[i] = event.values[i];
+                    }
+                    break;
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
 
     private final LocationListener locationListener = new LocationListener() {
 
